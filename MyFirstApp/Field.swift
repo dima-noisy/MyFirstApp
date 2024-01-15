909001a942e23254c603c5a6e3b29a396c226b28
Dmitry Poryadin <dporyadin11@gmail.com>
2024-01-15 23:57:00 +0300

    initial commit

---
+++ MyFirstApp/Field.swift
@@ -0,0 +1,16 @@
+//
+//  Field.swift
+//  MyFirstApp
+//
+//  Created by Дмитрий Порядин on 14.01.2024.
+//
+
+import UIKit
+
+class FieldView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = .systemPurple
+    }
+}
